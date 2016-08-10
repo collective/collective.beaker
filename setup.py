6ff791ae0c85cb7c97fe751d5e17dfbe94ae29d2
Maurits van Rees <maurits@vanrees.org>
2016-08-10 23:01:35 +0200

    Reindented setup.py

--- setup.py
+++ setup.py
@@ -3,38 +3,39 @@ import os
 
 version = '1.0b3'
 
-setup(name='collective.beaker',
-      version=version,
-      description="Beaker integration for Zope and Plone",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      classifiers=[
+setup(
+    name='collective.beaker',
+    version=version,
+    description="Beaker integration for Zope and Plone",
+    long_description=open("README.rst").read() + "\n" +
+                     open("CHANGES.rst").read(),
+    classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='beaker zope plone caching sessions',
-      author='Martin Aspeli',
-      author_email='optilude@gmail.com',
-      url='http://pypi.python.org/pypi/collective.beaker',
-      license='BSD',
-      packages=find_packages('src', exclude=['ez_setup']),
-      package_dir = {'':'src'},
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Beaker',
-          'zope.interface',
-          'zope.component',
-          'Zope2',
-          'zope.publisher',
-      ],
-      extras_require={
-        'Zope2.10': ['ZPublisherEventsBackport'],
-        'tests': ['Products.PloneTestCase', 'collective.testcaselayer']
-      },
-      entry_points="""
-      """,
-      )
+    ],
+    keywords='beaker zope plone caching sessions',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='https://pypi.python.org/pypi/collective.beaker',
+    license='BSD',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir = {'':'src'},
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Beaker',
+        'zope.interface',
+        'zope.component',
+        'Zope2',
+        'zope.publisher',
+    ],
+    extras_require={
+      'Zope2.10': ['ZPublisherEventsBackport'],
+      'tests': ['Products.PloneTestCase', 'collective.testcaselayer']
+    },
+    entry_points="""
+    """,
+    )
